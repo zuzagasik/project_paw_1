2701640be57882c8176ae6258927afb5097a3bdc
zuzagasik <zuzagasik@gmail.com>
2024-12-03 11:23:58 +0100

    lab_04

--- folder_projektu/folder_aplikacji/models.py
+++ folder_projektu/folder_aplikacji/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 # deklaracja statycznej listy wyboru do wykorzystania w klasie modelu
 MONTHS = models.IntegerChoices('Miesiace', 'Styczeń Luty Marzec Kwiecień Maj Czerwiec Lipiec Sierpień Wrzesień Październik Listopad Grudzień')
+PLCIE = models.IntegerChoices('PLEC', 'Kobieta Mężczyzna Inna')
 
 SHIRT_SIZES = (
         ('S', 'Small'),
@@ -23,10 +24,35 @@ class Team(models.Model):
 class Person(models.Model):
 
     name = models.CharField(max_length=60)
-    pseudonim = models.CharField(max_length = 100)
+    pseudonim = models.CharField(max_length = 80, default="")
     shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES, default=SHIRT_SIZES[0][0])
     month_added = models.IntegerField(choices=MONTHS.choices, default=MONTHS.choices[0][0])
     team = models.ForeignKey(Team, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
+class Osoba(models.Model):
+    PLEC_CHOICES = (
+        ("K", "Kobieta"),
+        ("M", "Mężczyzna"),
+        ("I", "Inna"),
+    )
+    
+    imie = models.CharField(max_length=40, blank = False, null = False)
+    nazwisko = models.CharField(max_length=60, blank = False, null = False)
+    plec = models.IntegerField(choices=PLCIE.choices, default=PLCIE.choices[2][0])
+    stanowisko = models.ForeignKey('Stanowisko', on_delete = models.CASCADE)
+    data_dodania = models.DateField(auto_now_add = True, editable = False)
+    
+    def __str__(self):
+        return f'{self.imie} {self.nazwisko}' 
+    
+    class Meta:
+        ordering = ["nazwisko"]
+
+class Stanowisko(models.Model):
+    nazwa = models.CharField(max_length=80, blank = False, null = False)
+    opis = models.TextField(blank = False, null = False)
+    
+    def __str__(self):
+        return self.nazwa
