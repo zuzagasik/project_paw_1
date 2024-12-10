4115376481af9cbc263be578c6e7753b9461dbcc
zuzagasik <zuzagasik@gmail.com>
2024-12-10 11:47:44 +0100

    lab_06

---
+++ folder_projektu/folder_aplikacji/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import Person, Team, MONTHS, SHIRT_SIZES
+
+
+class PersonSerializer(serializers.Serializer):
+
+    # pole tylko do odczytu, tutaj dla id działa też autoincrement
+    id = serializers.IntegerField(read_only=True)
+
+    # pole wymagane
+    name = serializers.CharField(required=True)
+
+    # pole mapowane z klasy modelu, z podaniem wartości domyślnych
+    # zwróć uwagę na zapisywaną wartość do bazy dla default={wybór}[0] oraz default={wybór}[0][0]
+    # w pliku models.py SHIRT_SIZES oraz MONTHS zostały wyniesione jako stałe do poziomu zmiennych skryptu
+    # (nie wewnątrz modelu)
+    shirt_size = serializers.ChoiceField(choices=SHIRT_SIZES, default=SHIRT_SIZES[0][0])
+    miesiac_dodania = serializers.ChoiceField(choices=MONTHS.choices, default=MONTHS.choices[0][0])
+
+    # odzwierciedlenie pola w postaci klucza obcego
+    # przy dodawaniu nowego obiektu możemy odwołać się do istniejącego poprzez inicjalizację nowego obiektu
+    # np. team=Team({id}) lub wcześniejszym stworzeniu nowej instancji tej klasy
+    team = serializers.PrimaryKeyRelatedField(queryset=Team.objects.all(), allow_null=True)
+
+    pseudonim = serializers.CharField(max_length=100, default="", allow_blank=True)
+
+    # przesłonięcie metody create() z klasy serializers.Serializer
+    def create(self, validated_data):
+        return Person.objects.create(**validated_data)
+
+    # przesłonięcie metody update() z klasy serializers.Serializer
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.shirt_size = validated_data.get('shirt_size', instance.shirt_size)
+        instance.miesiac_dodania = validated_data.get('miesiac_dodania', instance.miesiac_dodania)
+        instance.team = validated_data.get('team', instance.team)
+        instance.pseudonim = validated_data.get('pseudonim', instance.pseudonim)
+        instance.save()
+        return instance
